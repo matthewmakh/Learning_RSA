b2f9b01560ab23c0e1fb93bc3150c0f7e03cf708
matthewmakh <matthewmakh@gmail.com>
2025-01-20 16:18:13 -0500

    first commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from Crypto.Util.number import getPrime
+
 def EA(num1, num2):
     if num2 > num1:
         num1, num2 = num2, num1
@@ -13,6 +15,22 @@ def EA(num1, num2):
         num2 = remainder
 
         #print(f'{num1} mod {num2} = {num1%num2}')
+
+def EEA(num1,num2):
+    #extended euclidean algo
+    if num2 == 0:
+        return num1, 1, 0
+    gcd, x1, y1 = EEA(num2, num1 % num2)
+    x = y1
+    y = x1 - (num1 // num2) * y1
+    return gcd, x, y
+
+def mod_inverse(e, r):
+    gcd, x, _ = EEA(e, r)
+    if gcd != 1:
+        raise ValueError("e and r must be coprime to calculate modular inverse.")
+    return x % r
+
 def OPhi(num):
     count = 0
     for i in range(1,num):
@@ -21,25 +39,31 @@ def OPhi(num):
 
     return count
 
-def mod_inverse(e, r):
+'''def mod_inverse(e, r):
     if EA(e, r) == 1:
         return((e ** (OPhi(r) - 1) % r))
     else:
-        print("Cannot calculate mod inverse")
+        print("Cannot calculate mod inverse")'''
 
 def RSA_encrypt(m,e,n):
-    return (m**e) % n
+    #return (m**e) % n
+    # changed to this for efficiency
+    return pow(m,e,n)
 
 def RSA_decrypt(c,d,n):
-    return((c**d) %n)
+    #return((c**d) %n)
+    # changed to this for efficiency
+    return pow(c,d,n)
 
 
-p = 11
-q = 3
+p = getPrime(2048)
+q = getPrime(2048)
+e = 65537
 n = p*q
 r = (p-1)*(q-1)
-e = 3
 d = mod_inverse(e,r)
 
-print(RSA_encrypt(7,e,n))
-print(RSA_decrypt(RSA_encrypt(7,e,n),d,n))
+message = getPrime(128)
+
+print(RSA_encrypt(message,e,n))
+print(RSA_decrypt(RSA_encrypt(message,e,n),d,n))
